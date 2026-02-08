38cdea6708701bd639a3510a7859aeb2613551e9
Kuman37 <kuandykov5122@gmail.com>
2026-02-08 23:20:11 +0500

    Initial commit

---
+++ src/main/java/org/example/crowdfunding_endterm_project/CrowdfundingEndtermProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.crowdfunding_endterm_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdfundingEndtermProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdfundingEndtermProjectApplication.class, args);
+    }
+
+}
